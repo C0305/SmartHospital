4aa079cd3019b633b62afc53b253d364679dd2ff
C0305 <c0305@cobos.xyz>
2019-01-14 23:37:17 -0600

    Super componente de tablas en nuestro sistema

---
+++ resources/js/ui/components/bacabTables.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="box">
+        <div class="box__header">
+
+        </div>
+        <div class="box__body">
+            <el-table
+                    :data="dataArray"
+                    border>
+                <el-table-column
+                        v-for="(header,index) in tableConfig[1]"
+                        :key="index"
+                        :label="header.name"
+                        :prop="header.prop"
+                        :width="header.width"
+                        :fixed="header.fixed">
+
+                </el-table-column>
+                <el-table-column
+                        v-if="tableConfig[0].acciones === true"
+                        label="Acciones">
+                    <slot :name="tableConfig[0].accionesSlot"></slot>
+                </el-table-column>
+            </el-table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            tableConfig: Array,
+            dataArray: Array,
+        },
+        name: "bacabTables"
+    }
+</script>
+
+<style scoped>
+
+</style>
