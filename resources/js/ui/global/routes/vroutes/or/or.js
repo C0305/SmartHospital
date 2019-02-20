b85e965a76d6332ff688fb9207f6de7dc35134b5
C0305 <c0305@cobos.xyz>
2019-02-20 07:02:21 -0600

    subroute fix

--- resources/js/ui/global/routes/vroutes/or/or.js
+++ resources/js/ui/global/routes/vroutes/or/or.js
@@ -2,12 +2,5 @@ import { layouts } from "./layouts";
 import procedureTypes from './paths/procedures-types';
 
 export default () => [
-    {
-        path: 'or/types',
-        component: layouts.proceduresTypes,
-        props: route => ({
-            path: 'or/types',
-            back: false
-        })
-    }
+    ...procedureTypes(layouts)
 ]
